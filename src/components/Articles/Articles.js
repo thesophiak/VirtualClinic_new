72155d2f357b1de7cbf965124f750cc417d0bea1
Sophia Kapchinsky <s.kapchinsky@gmail.com>
2024-02-02 12:26:44 -0500

    Adjusted the history list and added delete functionality

--- src/components/Articles/Articles.js
+++ src/components/Articles/Articles.js
@@ -1,32 +1,47 @@
 import { useState, useEffect } from "react";
-import axios from 'axios'
+import axios from "axios";
 
+function Articles({ articleTopic }) {
+  const urlNyt = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+  const key = process.env.REACT_APP_NYT_KEY;
 
-function Articles (){
-  const urlNyt = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
-  const key= process.env.REACT_APP_NYT_KEY
+  const articleTopicDashed = articleTopic.split(" ").join("-");
+  console.log("dashedText", articleTopicDashed);
 
-  const [articles, setArticles]=useState("");
-    
-  useEffect(()=>{
-      const fetchArticles = async ()=>{
-          const response = await axios.get(`${urlNyt}?q=back-pain&api-key=${key}`)
-          console.log(response.data.response.docs)
-          console.log(response.data.response.docs[3])
-          console.log(response.data.response.docs[4])
-          console.log(response.data.response.docs[5])
-          
-          setArticles(response.data.response.docs[2])
-      }
-      fetchArticles()
-  }, []  )
+  console.log("article topic", articleTopic);
 
-  return(
+  const [articles, setArticles] = useState("");
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      const response = await axios.get(
+        `${urlNyt}?q=${articleTopic}&api-key=${key}`
+      );
+      console.log(" articles 2", response.data.response.docs);
+
+      setArticles(response.data.response.docs);
+    };
+    fetchArticles();
+  }, [articleTopicDashed]);
+
+  console.log(" articles", articles);
+
+  return (
     <>
-    <h1> Articles section</h1>
-    <div className="card__name"></div>
+      <h1> Articles section</h1>
+      <div className="card">
+        {articles.map((item, index) => (
+          <div key={index}>
+
+            <div className="card__name">{item.headline.main}</div>
+            {/* <div className="card__author">{item.byline?.original}</div> */}
+    
+           
+          </div>
+        ))}
+      </div>
     </>
-  )
+  );
 }
 
 export default Articles;
--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -3,45 +3,70 @@ import OpenAI from "openai";
 import Header from "../../components/Header/Header";
 import Articles from "../../components/Articles/Articles";
 import "./MainPage.scss";
+import axios from "axios";
 
 function MainPage() {
   let newQuestion = "";
+  const apiBE = `http://localhost:5000/`;
 
+  // useSTATE
   const [answer, setAnswer] = useState("");
-  console.log("answer", answer);
+  const [question, setQuestion] = useState("");
+  const [history, setHistory] = useState("");
+  const [historyList, setHistoryList] = useState([]);
 
-  const[question, setQuestion] = useState("")
-
-  const [history, setHistory] = useState([])
-  console.log("history: ", history)
-
-  const handleSubmit = (event) => {
+  // BUTTONS
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    let person = "cat";
+    let person = "dog";
 
     newQuestion = event.target.question.value;
-    console.log(newQuestion);
-    getAnswer(newQuestion, person);
-    getHistory(newQuestion)
+    console.log("new Qustion:", newQuestion);
+
+    const newAnswer = await getAnswer(newQuestion, person);
+   const newHistory =  await getHistory(newQuestion);
+
+    const newDataEntry = {
+      question: newQuestion,
+      history: newHistory,
+      answer: newAnswer,
+    };
+    console.log("req bod:", newDataEntry);
+
+    setQuestion(newQuestion);
+    setAnswer(newAnswer);
+    setHistory(newHistory);
+
+    axios
+      .post(`${apiBE}`, newDataEntry)
+      .then((response) => {
+        fetchHistoryList();
+      })
+      .catch((error) => {
+        console.error("Error adding data to DB:", error);
+      });
   };
 
-  const handleReset = (event) =>{
+  const handleReset = (event) => {
     event.preventDefault();
-    setQuestion("")
+    setQuestion("");
     setAnswer("");
-  }
+  };
 
-  const handleClearHistory = (event) =>{
-    event.preventDefault();
-    setHistory([])
-  }
+  const handleClearHistory = async () => {
+    try {
+      await axios.delete(`${apiBE}`)
+      fetchHistoryList();
+      setQuestion("");
+      setAnswer("");
+      // alert("History cleared successfully");
+    }catch(error){
+      console.error("Error clearing history:", error);
+    }
+  };
 
-  // const handleHistoryClick = (index) => {
-  //   const selectedHistory = history[index];
-  //   setQuestion(selectedHistory.question);
-  //   setAnswer(selectedHistory.answer);
-  // }
 
+  // API call to chatGTP
   const openai = new OpenAI({
     apiKey: process.env.REACT_APP_OPEN_AI_KEY,
     dangerouslyAllowBrowser: true,
@@ -60,9 +85,10 @@ function MainPage() {
           { role: "user", content: prompt },
         ],
       });
-      setAnswer(response.choices[0].message.content);
+      return response.choices[0].message.content;
     } catch (error) {
       console.error("Error fetching answers:", error);
+      return "";
     }
   };
 
@@ -73,24 +99,35 @@ function MainPage() {
         messages: [
           {
             role: "system",
-            content: "Summarize the prompt in 3 words or less. Make it sentence case. Do not include period at the end of the phrase. ",
+            content:
+              "Summarize the prompt in 3 words or less. Make it sentence case. Do not include period at the end of the phrase. ",
           },
 
           { role: "user", content: prompt },
         ],
       });
-
-      const historyItem = {
-        question: prompt,
-        answer: response.choices[0].message.content,
-      };
-
-      setHistory((prevHistory)=>[...prevHistory, response.choices[0].message.content]);
+      return response.choices[0].message.content;
     } catch (error) {
       console.error("Error fetching answers:", error);
+      return "";
     }
   };
 
+  // Get HISTORY LIST
+  useEffect(()=>{
+    fetchHistoryList();
+  },[]);
+
+  const fetchHistoryList = async () =>{
+    try{
+      const response = await axios.get(`${apiBE}`)
+      setHistoryList(response.data);
+    }catch(error){
+      console.error("Error fetching history list:", error);
+    }
+  }
+
+console.log("History list", historyList)
   return (
     <>
       <Header />
@@ -104,8 +141,8 @@ function MainPage() {
                 className="questions"
                 placeholder="Enter your question here"
                 name="question"
-                value={question}      
-                onChange={(event)=> setQuestion(event.target.value)}
+                value={question}
+                onChange={(event) => setQuestion(event.target.value)}
               ></textarea>
             </div>
             <div>
@@ -119,9 +156,9 @@ function MainPage() {
           <div className="title__answer">Advice from your virtual doctor</div>
           <div className="answer">{answer}</div>
           <div>
-            <button className="button__another"
-            onClick={handleReset}
-            >I have another question</button>
+            <button className="button__another" onClick={handleReset}>
+              I have another question
+            </button>
           </div>
         </section>
 
@@ -130,20 +167,17 @@ function MainPage() {
           <div className="title__history">Question history</div>
           <div className="history">
             <ul>
-              {history.map((item, index)=> (
-                <li key={index}
-                // onClick={()=>handleHistoryClick(index)}
-                className="history-item"
-                >{item}</li>
+              {historyList.map((item)=>(
+                <li key ={item.id}>{item.history}</li>
               ))}
             </ul>
           </div>
-          <button className="button__clear"
-          onClick={handleClearHistory}
-          >Clear history</button>
+          <button className="button__clear" onClick={handleClearHistory}>
+            Clear history
+          </button>
         </section>
       </main>
-      <Articles />
+      <Articles articleTopic={history} />
     </>
   );
 }
